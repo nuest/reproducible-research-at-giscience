4c016204cf9ead69813944d3f3d556c8a54c79b2
Daniel Nüst <daniel.nuest@uni-muenster.de>
2020-04-24 13:51:17 +0200

    fix required R packages

--- install.R
+++ install.R
@@ -1,19 +1,28 @@
 # Install CRAN packages
+install.packages("pdftools")
 install.packages("tidyverse")
+install.packages("tidytext")
 install.packages("ggthemes")
 install.packages("kableExtra")
 install.packages("huxtable")
 install.packages("here")
 install.packages("gridBase")
 install.packages("gridExtra")
-install.packages("googlesheets4")
 install.packages("reshape2")
+install.packages("wordcloud")
+install.packages("quanteda")
+# not available (in required version) for R 3.6.0 > install tag from GitHub
+remotes::install_github("tidyverse/googledrive", ref = "v1.0.0", upgrade = "never")
+remotes::install_github("tidyverse/googlesheets4", ref = "v0.1.1", upgrade = "never")
+
+# https://github.com/rstudio/rticles/pull/288
+remotes::install_github("nuest/rticles", ref = "LIPIcs")
 
 # Install LaTeX packages
-tinytex::tlmgr_install("siunitx")
-tinytex::tlmgr_install("xcolor")
-tinytex::tlmgr_install("colortbl")
-tinytex::tlmgr_install("multirow")
-tinytex::tlmgr_install("lscape")
-tinytex::tlmgr_install("wrapfig")
-tinytex::tlmgr_install("float")
+#tinytex::tlmgr_install("siunitx")
+#tinytex::tlmgr_install("xcolor")
+#tinytex::tlmgr_install("colortbl")
+#tinytex::tlmgr_install("multirow")
+#tinytex::tlmgr_install("lscape")
+#tinytex::tlmgr_install("wrapfig")
+#tinytex::tlmgr_install("float")
